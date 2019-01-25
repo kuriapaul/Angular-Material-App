29d6882a811ee3965b96eddc060b8bb9a2bb05fe
kuriapaul <pauwains@gmail.com>
2019-01-25 23:40:03 +0300

    enabled data deletion

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import {DataService} from '../data/data.service';
 import {Post} from '../Post';
 import {DataSource} from '@angular/cdk/table';
 import {Observable} from 'rxjs/Observable';
+import { AuthService } from '../auth.service';
 
 
 @Component({
@@ -12,13 +13,22 @@ import {Observable} from 'rxjs/Observable';
 })
 export class DashboardComponent {
 
-  constructor(private dataService: DataService) {
+  constructor(private dataService: DataService, public auth: AuthService) {
   }
 
   displayedColumns = ['date_posted', 'title', 'category', 'delete'];
   dataSource = new PostDataSource(this.dataService);
-}
 
+
+deletePost(id) {
+  if (this.auth.isAuthenticated()) {
+    this.dataService.deletePost(id);
+    this.dataSource = new PostDataSource(this.dataService);
+  } else {
+    alert('Login in Before');
+  }
+}
+}
 export class PostDataSource extends DataSource<any> {
   constructor(private dataService: DataService) {
     super();
